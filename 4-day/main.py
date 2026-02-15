1b63777daf1f672bd26ca36b278dbf85e5c288ef
Eddynilson <qeddynilson@gmail.com>
2026-02-15 17:59:59 +0100

    Modified: 4-day/main.py

--- 4-day/main.py
+++ 4-day/main.py
@@ -1,18 +1,19 @@
 import random
 user_choose = int(input("what so you choose? Type 0 for Rock, 1 for Paper or 2 for Scissorsa\n"))
 list = ["Rock", "Paper", "Scissorsa"]
-print(f"{list[user_choose]}")
-maching_choose = random.randint(0, 2)
-print(f"Computer Choose:\n{lista[maching_choose]}")
-if maching_choose == user_choose:
-	print("It's a draw")
-elif user_choose == 0 and maching_choose == 2:
-	print("You win")
-elif user_choose == 1 and maching_choose == 0:
-	print("You win")
-elif user_choose == 2 and maching_choose == 1:
-	print("You win")
-elif user_choose != 0 or user_choose != 1 or use_choose != 2:
-	print("You lose")
+if user_choose == 0 or user_choose == 1 or user_choose == 2:
+	print(f"{list[user_choose]}")
+	maching_choose = random.randint(0, 2)
+	print(f"Computer Choose:\n{list[maching_choose]}")
+	if maching_choose == user_choose:
+		print("It's a draw")
+	elif user_choose == 0 and maching_choose == 2:
+		print("You win")
+	elif user_choose == 1 and maching_choose == 0:
+		print("You win")
+	elif user_choose == 2 and maching_choose == 1:
+		print("You win")
+	else:
+		print("You lose")
 else:
-	print("Error: Write 0, 1 or 2")
+	print(f"Error: Your choose is not valid.")
